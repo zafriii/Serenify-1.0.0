a9038f363f0cfde98759e1eeba616750a4c1bd5c
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-12-19 11:37:48 +0600

    Update feedbackRouter.js

--- backend/server/router/feedbackRouter.js
+++ backend/server/router/feedbackRouter.js
@@ -3,7 +3,6 @@ const { submitFeedback } = require('../controllers/feedbackController');
 
 const router = express.Router();
 
-// POST route for submitting feedback
 router.post('/', submitFeedback);
 
 module.exports = router;
