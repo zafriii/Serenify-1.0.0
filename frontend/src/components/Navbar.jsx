81196aec6aad55f2707321adf8a96575bb03d68e
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2025-01-09 00:20:55 +0600

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,17 +3,26 @@ import { NavLink } from 'react-router-dom';
 import './styles/navbar.css';
 import NotificationBell from './NotificationBel';
 import { useAuth } from '../store/Auth';
-
+import { IoMdArrowDropdown } from "react-icons/io";
 
 function Navbar() {
-
-  
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
+  const [dropdownOpen, setDropdownOpen] = useState({
+    chats: false,
+    medication: false,
+  });
+
   const { isLoggedin, LogoutUser } = useAuth();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleDropdown = (menu) => {
+    setDropdownOpen((prev) => ({
+      ...prev,
+      [menu]: !prev[menu],
+    }));
   };
 
   const handleLogout = () => {
@@ -23,63 +32,77 @@ function Navbar() {
 
   return (
     <div className="navbar">
-    
-    <div className="navlogo">
+      <div className="navlogo">
         <NavLink to="/welcome">
-            <img src="/images/site-logo.png" alt="Logo" />
+          <img src="/images/site-logo.png" alt="Logo" />
         </NavLink>
-
-    </div>
-
-
+      </div>
 
       <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
-        
-        <NavLink to="/chats">
-            <button className="button" onClick={toggleMenu}>
-              <span>Chats</span>
-            </button>
-          </NavLink>
+        {/* Chats Dropdown */}
+        <div
+          className={`dropdown ${dropdownOpen.chats ? 'open' : ''}`}
+          onMouseEnter={() => toggleDropdown('chats')}
+          onMouseLeave={() => toggleDropdown('chats')}
+        >
+          <button className="button">
+            <span>Chats <IoMdArrowDropdown /></span>
+          </button>
+          <div className="dropdown-menu">
+            <NavLink to="/chats" onClick={toggleMenu}>
+              <span>Chat with AI</span>
+            </NavLink>
+            <NavLink to="/community" onClick={toggleMenu}>
+              <span>Chat Anonymously</span>
+            </NavLink>
+          </div>
+        </div>
 
+        {/* Medication Dropdown */}
+        <div
+          className={`dropdown ${dropdownOpen.medication ? 'open' : ''}`}
+          onMouseEnter={() => toggleDropdown('medication')}
+          onMouseLeave={() => toggleDropdown('medication')}
+        >
+          <button className="button">
+            <span>Medication <IoMdArrowDropdown /></span>
+          </button>
+          <div className="dropdown-menu">
+            <NavLink to="/medication" onClick={toggleMenu}>
+              <span>Explore Tools</span>
+            </NavLink>
+            <NavLink to="/therapists" onClick={toggleMenu}>
+              <span>Our Professionals</span>
+            </NavLink>
+          </div>
+        </div>
 
-          <NavLink to="/medication">
-            <button className="button" onClick={toggleMenu}>
-              <span>Medication</span>
-            </button>
-          </NavLink>
-
-          
-
-          <NavLink to="/manifest">
-            <button className="button" onClick={toggleMenu}>
-              <span>Manifest</span>
-            </button>
-          </NavLink>
-
-          <NavLink to="/posts">
-            <button className="button" onClick={toggleMenu}>
-              <span>Posts</span>
-            </button>
-          </NavLink>          
-
-      {isLoggedin ? (
-
-        <>
-
-        <NavLink to="/profile">
+        <NavLink to="/manifest">
           <button className="button" onClick={toggleMenu}>
-            <span>Profile</span>
+            <span>Manifest</span>
           </button>
         </NavLink>
 
-          <NavLink to="/login">
-          <button className="button" onClick={handleLogout}>
-            <span>Logout</span>
+        <NavLink to="/posts">
+          <button className="button" onClick={toggleMenu}>
+            <span>Posts</span>
           </button>
         </NavLink>
 
-        </>
+        {isLoggedin ? (
+          <>
+            <NavLink to="/profile">
+              <button className="button" onClick={toggleMenu}>
+                <span>Profile</span>
+              </button>
+            </NavLink>
 
+            <NavLink to="/login">
+              <button className="button" onClick={handleLogout}>
+                <span>Logout</span>
+              </button>
+            </NavLink>
+          </>
         ) : (
           <NavLink to="/login">
             <button className="button" onClick={toggleMenu}>
@@ -88,10 +111,7 @@ function Navbar() {
           </NavLink>
         )}
 
-
-       <NotificationBell/>
-    
-       
+        <NotificationBell />
       </div>
 
       <div className="menu-toggle" onClick={toggleMenu}>
